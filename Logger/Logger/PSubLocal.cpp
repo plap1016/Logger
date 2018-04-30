771a7ee829f42cfd31502f942e2c12ab15589b93
andrew.mornement <devnull@localhost>
2018-04-30 13:39:31 +1000

    Update grafted components to include new features

--- Logger/Logger/PSubLocal.cpp
+++ Logger/Logger/PSubLocal.cpp
@@ -139,7 +139,7 @@ void PSubLocal::processMsg(const PubSub::Message& m)
 	std::string base64(it_base64_t(m.payload.begin()), it_base64_t(m.payload.end()));
 	base64.append(writePaddChars, '=');
 
-	m_strm << tdiff.count() << " " << m.ttl << " 0 " << PubSub::toString(m.subject) << " " << base64 << std::endl;
+	m_strm << tdiff.count() << " " << m.age << " " << m.ttl << " " << PubSub::toString(m.subject) << " " << base64 << std::endl;
 
 	if (++m_evtCount >= m_disp.cfg().MaxEventCount())
 	{
